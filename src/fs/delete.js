8f2c957a66c84bb74429a4840e6653edee1846f2
Alex <sivkov@inbox.ru>
2022-06-08 21:04:30 +0500

    feat: add delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,17 @@
+import { rm } from 'fs/promises';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(__fileName);
+
 export const remove = async () => {
-    // Write your code here 
+  const src = __dirname + '/files/fileToRemove.txt';
+  try {
+    await rm(src);
+  } catch (err) {
+    throw new Error('FS operation failed');
+  }
 };
+
+remove();
