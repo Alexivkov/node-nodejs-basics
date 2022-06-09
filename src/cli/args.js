b73076694341cd794abec9f10ad144db975f0c5a
Alex <sivkov@inbox.ru>
2022-06-09 09:38:25 +0500

    feat: add cli functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,17 @@
 export const parseArgs = () => {
-    // Write your code here 
+  const userInputArguments = process.argv.slice(2);
+  
+  const cliArguments = userInputArguments.reduce((acc, arg, index, arr) => {
+    const valueCandidate = arr[index + 1];
+    if (valueCandidate && arg.startsWith('--')){
+      const transformArgs = arg.slice(2);
+      const cliArgumentsTransformed = `${transformArgs} is ${valueCandidate}`;
+      acc.push(cliArgumentsTransformed);
+    } 
+    return acc;
+  }, []);
+
+  console.log(cliArguments.join(', '));
 };
+
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,10 @@
 export const parseEnv = () => {
-    // Write your code here 
+  const allVariablesArray = Object.entries(process.env);
+  const rssVariablesArray = allVariablesArray.reduce((acc, [key, value]) => {
+    if (key.startsWith('RSS_')) acc.push(`${key}=${value}`);
+    return acc;
+  }, []);
+  console.log(rssVariablesArray.join('; '))
 };
+
+parseEnv();
