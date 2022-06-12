701b74df7d08b78deebf523c3b8ca182e07e9fc0
Alex <sivkov@inbox.ru>
2022-06-12 09:06:31 +0500

    feat: add calcHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,19 @@
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const fileContent = await readFile(__dirname + '/files/fileToCalculateHashFor.txt');
+    const hash = createHash('sha256').update(fileContent).digest('hex');
+    console.log(`Hash: ${hash}`);
+  } catch (error) {
+    throw error;
+  }
 };
+
+calculateHash();
