edff4a084eea077f68ddf65a8dcba06fde1cfb3b
Alex <sivkov@inbox.ru>
2022-06-13 06:41:20 +0500

    feat: add cp function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,15 @@
+import { spawn } from 'child_process';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const chProcess = spawn('node', ['--version']);
+  chProcess.stdout.on('data', (data) => {
+    console.log(data.toString());
+  })
 };
+
+spawnChildProcess();
